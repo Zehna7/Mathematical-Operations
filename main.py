0e13ff3ebfaf20079128b823f3da9924d1f57a37
Zehna7 <zehna.omar2012@gmail.com>
2024-10-12 18:24:07 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import math
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+
+print(math.ceil(45.490))
+print(math.floor(45.872))
+print(math.factorial(6))
+
+x = 10
+y = -15
+print("The value of x after copying the sign from y is: " + str(math.copysign(x,y)))
+
+print("The GCD of 24 and 56: " + str(math.gcd(24,56)))
+
+print("Absolute value of -96 and 56 are: " , str(math.fabs(-96))) ,  str(math.fabs(56))
